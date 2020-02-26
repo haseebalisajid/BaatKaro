2e5ced206dd9a52570efe340008bfc97f0a564f6
haseebalisajid <alihaseeb714@gmail.com>
2020-02-26 22:36:00 +0500

    Web Chat App

---
+++ Scripts/firebase.init.js
@@ -0,0 +1,12 @@
+﻿// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDejeg2AC2WPFWJwCNuSo766wYJijWCQCc",
+    authDomain: "chat-app-9ab99.firebaseapp.com",
+    databaseURL: "https://chat-app-9ab99.firebaseio.com",
+    projectId: "chat-app-9ab99",
+    storageBucket: "chat-app-9ab99.appspot.com",
+    messagingSenderId: "938353661863",
+    appId: "1:938353661863:web:ce0d6774295195d6f94882"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
