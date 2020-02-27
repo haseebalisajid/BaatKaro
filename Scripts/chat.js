926d86a81ddd7ef31147dae8a6936668947e986e
haseebalisajid <alihaseeb714@gmail.com>
2020-02-27 14:00:08 +0500

    Web Chat App

--- Scripts/chat.js
+++ Scripts/chat.js
@@ -93,7 +93,7 @@ loadAllEmoji();
 
 function loadAllEmoji() {
     var emoji = '';
-    for (var i = 128512; i <= 128566; i++) {
+    for (var i = 100; i <= 128566; i++) {
         emoji += `<a href="#" style="font-size: 22px;" onclick="getEmoji(this)">&#${i};</a>`;
     }
 
