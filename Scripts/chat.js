4e2d36c8648ce94f5c069cd40edd47d8a4e3f141
haseebalisajid <alihaseeb714@gmail.com>
2020-02-29 16:49:28 +0500

    Web Chat App

--- Scripts/chat.js
+++ Scripts/chat.js
@@ -336,7 +336,7 @@ function LoadChatList() {
                                 </div>
                                 <div class="col-md-10" style="cursor:pointer;">
                                     <div class="name">${user.name}</div>
-                                    <div class="under-name"><hr></hr></div>
+                                    <div class="under-name"><hr style="width:10px;></hr></div>
                                 </div>
                             </div>
                         </li>`;
