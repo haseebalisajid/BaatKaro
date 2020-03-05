47f76b141c7770eac55c599f75499dfbe4b9f643
haseebalisajid <alihaseeb714@gmail.com>
2020-03-06 00:13:46 +0500

    Web Chat App

--- Scripts/setting.js
+++ Scripts/setting.js
@@ -1,3 +1,16 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDejeg2AC2WPFWJwCNuSo766wYJijWCQCc",
+    authDomain: "chat-app-9ab99.firebaseapp.com",
+    databaseURL: "https://chat-app-9ab99.firebaseio.com",
+    projectId: "chat-app-9ab99",
+    storageBucket: "chat-app-9ab99.appspot.com",
+    messagingSenderId: "938353661863",
+    appId: "1:938353661863:web:ce0d6774295195d6f94882"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
 let id=firebase.auth().currentUser;
 console.log(id + "sda");
 let ref = firebase.database().ref('/users/' + id).once('value').then(function(snapshot) {
