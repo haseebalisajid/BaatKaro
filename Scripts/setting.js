8eaedc0b0b3e3536474b083094aaa5e1f01bc69e
haseebalisajid <alihaseeb714@gmail.com>
2020-03-06 00:22:25 +0500

    Web Chat App

--- Scripts/setting.js
+++ Scripts/setting.js
@@ -1,9 +1,9 @@
 
 let id=firebase.auth().currentUser;
 console.log(id + "sda");
-let ref = firebase.database().ref('/users/' + id).once('value').then(function(snapshot) {
+let ref = firebase.database().ref('users/' + id).once('value').then(function(snapshot) {
     let userData = snapshot.val();
-    console.log(userData.email);
+    console.log(userData);
 });
 var userName=document.getElementById('name');
 var userStatus=document.getElementById('status');
