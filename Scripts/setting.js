346dfb7f780a11eabc439c50046424d5cc13924b
haseebalisajid <alihaseeb714@gmail.com>
2020-03-06 12:42:25 +0500

    Web Chat App

--- Scripts/setting.js
+++ Scripts/setting.js
@@ -3,7 +3,7 @@ var userEmail="";
 var userID="";
 var userPhoto="";
 var userStatus="";
-
+console.log(firebase.auth().currentUser);
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // User logged in already or has just logged in.
