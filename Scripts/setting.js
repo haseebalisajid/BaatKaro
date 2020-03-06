e1f506b6152c558b30df0f35b833697a564c0307
haseebalisajid <alihaseeb714@gmail.com>
2020-03-06 16:33:44 +0500

    Web Chat App

--- Scripts/setting.js
+++ Scripts/setting.js
@@ -6,7 +6,7 @@ var userStatus="";
 
 var leadsRef = firebase.database().ref('users');
 leadsRef.on('value', function(snapshot) {
-    console.log(snapshot.key);
+    console.log(snapshot);
     snapshot.forEach(function(childSnapshot) {
       var childData = childSnapshot.val();
       console.log(childData.email);
