691671e1343169d2472cca57a57d6ca9d6847608
haseebalisajid <alihaseeb714@gmail.com>
2020-03-07 23:11:14 +0500

    Web Chat App

--- Scripts/setting.js
+++ Scripts/setting.js
@@ -1,6 +1,44 @@
 var userName=document.getElementById('Name')
 var userStatus=document.getElementById('Status')
 var userId=""
+var userPot=""
 
-console.log(firebase.auth().currentUser.photoURL)
 
+firebase.auth().onAuthStateChanged(async firebaseUser => {
+    if(firebaseUser){
+        userId=firebaseUser.uid
+        await firebase.database().ref(`/users/${firebaseUser.uid}`).once('value').then(res => {
+            let data = res.val();
+            userPot=setValue(data.name,data.Status,data.photoURL)
+        });
+    }
+});
+
+function setValue(name,status,photo){
+    var usrName=document.getElementById('Name').innerHTML=name
+    var usrStatus=document.getElementById('Status').innerHTML=status
+    document.getElementById('imgProfile').src = photo
+    return photo
+}
+console.log(userId)
+function update(){
+    if(userName.value != "" && userStatus.value != ""){
+        document.getElementById('save').disabled=false;
+    }
+    else{
+        document.getElementById('save').disabled=true;
+    }
+}
+function changeVal(){
+    var Name=document.getElementById('name').value;
+    var stat=document.getElementById('getStatus').value;
+    firebase.database().ref(`/users/${userId}`).set({
+        name:Name,
+        Status:stat,
+        photoURL:userPot
+    })
+    
+}
+function back(){
+    window.location.href="/main.html"
+}
