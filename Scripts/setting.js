1a1739f05bb203125256bdbd3045461cdd8877b5
haseebalisajid <alihaseeb714@gmail.com>
2020-03-07 23:06:15 +0500

    Web Chat App

--- Scripts/setting.js
+++ Scripts/setting.js
@@ -4,38 +4,3 @@ var userId=""
 
 console.log(firebase.auth().currentUser.photoURL)
 
-firebase.auth().onAuthStateChanged(async firebaseUser => {
-    if(firebaseUser){
-        await firebase.database().ref(`/users/${firebaseUser.uid}`).once('value').then(res => {
-            let data = res.val();
-            setValue(data.name,data.Status,data.photoURL)
-        });
-    }
-});
-
-function setValue(name,status,photo){
-    var usrName=document.getElementById('Name').innerHTML=name
-    var usrStatus=document.getElementById('Status').innerHTML=status
-    document.getElementById('imgProfile').src = photo
-}
-console.log(userId)
-function update(){
-    if(userName.value != "" && userStatus.value != ""){
-        document.getElementById('save').disabled=false;
-    }
-    else{
-        document.getElementById('save').disabled=true;
-    }
-}
-function changeVal(){
-    var Name=document.getElementById('name').value;
-    var stat=document.getElementById('getStatus').value;
-    firebase.database().ref(`/users/${userId}`).set({
-        name:Name,
-        Status:stat
-    })
-    
-}
-function back(){
-    window.location.href="/main.html"
-}
