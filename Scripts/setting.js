1c5f4895d31dfeb9374fef68e5da91dc722410e9
haseebalisajid <alihaseeb714@gmail.com>
2020-03-07 15:58:59 +0500

    Web Chat App

--- Scripts/setting.js
+++ Scripts/setting.js
@@ -15,7 +15,7 @@ var userStatus="";
 
 firebase.auth().onAuthStateChanged(async firebaseUser => {
     if(firebaseUser){
-        console.log(firebase.auth().currentUser);
+        console.log(firebase.auth().currentUser.photoURL);
     }
 });
 document.getElementById('imgProfile').src = firebase.auth().currentUser.photoURL;
