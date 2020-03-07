6c8446765bfce04f292eb17be7d2bd85233f5106
haseebalisajid <alihaseeb714@gmail.com>
2020-03-07 21:46:42 +0500

    Web Chat App

--- Scripts/setting.js
+++ Scripts/setting.js
@@ -1,30 +1,39 @@
-var userName;
-var userPhoto;
-var userStatus;
+var userName="";
+var userEmail="";
+var userID="";
+var userPhoto="";
+var userStatus="";
 
-
-firebase.auth().onAuthStateChanged( firebaseUser => {
-    if(firebaseUser){
-        firebase.database().ref(`/users/${firebaseUser.uid}`).once('value').then( res => {
-            let data = res.val();
-            console.log(data.name,data.Status)
-            userName=data.name
-            userPhoto=data.photoURL
-            userStatus=data.Status
-        });
-    }
+var leadsRef = firebase.database().ref(`users/${firebaseUser.uid}`);
+leadsRef.once('value', function(snapshot) {
+    //console.log(snapshot.id);
+    snapshot.forEach(function(childSnapshot) {
+      var childData = childSnapshot.val();
+      console.log(childData);
+    });
 });
 
+// firebase.auth().onAuthStateChanged(async firebaseUser => {
+//     if(firebaseUser){
+//         console.log(firebaseUser.email);
+//         console.log(firebaseUser.uid);
+//         await firebase.database().ref(`/users/${firebaseUser.uid}`).once('value').then(res => {
+//             let data = res.val();
+//             console.log(data);
+//         });
+//     }
+// });
 
-console.log(userName,userPhoto,userStatus);
-// var userName=document.getElementById('name');
-// var userStatus=document.getElementById('status');
-// function update(){
-//     console.log(userName.value);
-//     if(userName.value != "" && userStatus.value != ""){
-//         document.getElementById('save').disabled=false;
-//     }
-//     else{
-//         document.getElementById('save').disabled=true;
-//     }
-// }
+
+//  console.log(userEmail,userName,userID);
+var userName=document.getElementById('name');
+var userStatus=document.getElementById('status');
+function update(){
+    console.log(userName.value);
+    if(userName.value != "" && userStatus.value != ""){
+        document.getElementById('save').disabled=false;
+    }
+    else{
+        document.getElementById('save').disabled=true;
+    }
+}
