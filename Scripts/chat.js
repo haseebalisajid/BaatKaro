4e13de86181baadc3b70cba064e804c28a6521f1
haseebalisajid <alihaseeb714@gmail.com>
2020-03-29 18:02:20 +0500

    Web Chat App

--- Scripts/chat.js
+++ Scripts/chat.js
@@ -349,17 +349,36 @@ function PopulateUserList() {
                                                          <span class="spinner-border text-primary mt-5" style="width:7rem;height:7rem"></span>
                                                      </div>`;
     var db = firebase.database().ref('users');
+    var dbNoti=firebase.database().ref('notification');
     var lst = '';
     db.on('value', function (users) {
         if (users.hasChildren()) {
             lst = `<li class="list-group-item" style="background-color:#f8f8f8;">
                             <input id="Search" type="text"  placeholder="Search or new chat" class="form-control form-rounded" />
                         </li>`;
+                        document.getElementById('lstUsers').innerHTML = lst;
         }
         users.forEach(function (data) {
             var user = data.val();
             if (user.email !== firebase.auth().currentUser.email) {
-                lst += `<li class="list-group-item list-group-item-action" data-dismiss="modal" >
+                dbNoti.orderByChild('sendTo').equalTo(data.key).on('value',function(noti){
+                    if(noti.numChildren()>0 && Object.values(noti.val()[0].sendFrom === currentUserKey)){
+                        lst = `<li class="list-group-item list-group-item-action">
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <img src="${user.photoURL}" class="rounded-circle friend-pic" />
+                                </div>
+                                <div class="col-md-10" style="cursor:pointer;">
+                                    <div class="name">${user.name}
+                                        <button class="btn btn-sm btn-primary" style="float:right;"><i class="fas fa-user-plus"></i>Sent</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>`;
+                        document.getElementById('lstUsers').innerHTML += lst;
+                    }
+                    else{
+                        lst = `<li class="list-group-item list-group-item-action" data-dismiss="modal" >
                             <div class="row">
                                 <div class="col-md-2">
                                     <img src="${user.photoURL}" class="rounded-circle friend-pic" />
@@ -371,10 +390,11 @@ function PopulateUserList() {
                                 </div>
                             </div>
                         </li>`;
+                        document.getElementById('lstUsers').innerHTML += lst;
+                    }
+                })
             }
         });
-
-        document.getElementById('lstUsers').innerHTML = lst;
     });
 
 }
@@ -383,9 +403,7 @@ function notificationCount(){
     let db=firebase.database().ref('notification');
 
     db.orderByChild('sendTo').equalTo(currentUserKey).on('value',function(noti){
-        let count=noti.numChildren();
-        console.log(count)
-        document.getElementById('notification').innerHTML=count;
+        document.getElementById('notification').innerHTML=noti.numChildren();;
     })
 }
 
@@ -402,7 +420,7 @@ function sendRequest(key){
             alert(error)
         }
         else{
-            
+            PopulateUserList()
         }
     })
 }
