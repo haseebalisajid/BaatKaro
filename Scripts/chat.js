61dafb9bc903951935cb146ec1c6ff6997043d24
haseebalisajid <alihaseeb714@gmail.com>
2020-03-29 20:13:13 +0500

    Web Chat App

--- Scripts/chat.js
+++ Scripts/chat.js
@@ -385,7 +385,7 @@ function PopulateUserList() {
                                 </div>
                                 <div class="col-md-10" style="cursor:pointer;">
                                     <div class="name">${user.name}
-                                        <button onclick="sendRequest('${data.key}')" class="btn btn-sm btn-primary" style="float:right;"><i class="fas fa-user-plus"></i>Send Request</button>
+                                        <button onclick="sendRequest('${data.key}')" class="btn btn-sm btn-primary" style="float:right;"><i class="fas fa-user-plus"></i> Send Request</button>
                                     </div>
                                 </div>
                             </div>
@@ -447,8 +447,8 @@ function PopulateNotifications(){
                                 </div>
                                 <div class="col-md-10" style="cursor:pointer;">
                                     <div class="name">${user.name}
-                                        <button onclick="Reject()" class="btn btn-sm btn-danger" style="float:right;margin-left:1%;"><i class="fas fa-user-times"></i>Reject</button>
-                                        <button onclick="Accept()" class="btn btn-sm btn-success" style="float:right;"><i class="fas fa-user-check"></i>Accept</button>
+                                        <button onclick="Reject()" class="btn btn-sm btn-danger" style="float:right;margin-left:1%;"><i class="fas fa-user-times"></i> Reject</button>
+                                        <button onclick="Accept()" class="btn btn-sm btn-success" style="float:right;"><i class="fas fa-user-check"></i> Accept</button>
                                     </div>
                                 </div>
                             </div>
