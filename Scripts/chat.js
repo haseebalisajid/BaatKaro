9e360486b946aabd04f240515eb31c57e1fc9f86
haseebalisajid <alihaseeb714@gmail.com>
2020-03-30 02:48:41 +0500

    Web Chat App

--- Scripts/chat.js
+++ Scripts/chat.js
@@ -613,7 +613,7 @@ function onStateChanged(user) {
             //         })
             //     });
 
-            document.getElementById('lnkNewChat').classList.remove('disabled');
+            // document.getElementById('lnkNewChat').classList.remove('disabled');
             document.getElementById('lnkSetting').classList.remove('disabled');
 
             LoadChatList();
