88e11a67cedb937aee0dc924100d7d9c0b231c69
haseebalisajid <alihaseeb714@gmail.com>
2020-03-30 18:45:46 +0500

    Web Chat App

--- Scripts/chat.js
+++ Scripts/chat.js
@@ -365,9 +365,9 @@ function PopulateUserList() {
             var user = data.val();
             if (user.email !== firebase.auth().currentUser.email) {
                 dbNoti.orderByChild('sendTo').equalTo(data.key).on('value',function(noti){
-                    noti.forEach(function(data){
-                        chk=data.val()
-                        if(noti.numChildren()>0 && Object.values(noti.val())[0].sendFrom === currentUserKey){
+                    if(noti.numChildren()>0 && Object.values(noti.val())[0].sendFrom === currentUserKey){
+                        noti.forEach(function(data){
+                            chk=data.val()
                             if(chk.status === "Accept"){
                                 lst += `<li class="list-group-item list-group-item-action">
                                     <div class="row">
@@ -398,63 +398,63 @@ function PopulateUserList() {
                                 </li>`;
                                 document.getElementById('lstUsers').innerHTML = lst;
                             }
-                        }
-                        else{
-                            dbNoti.orderByChild('sendFrom').equalTo(data.key).on('value',function(noti){
+                        })
+                    }
+                    else{
+                        dbNoti.orderByChild('sendFrom').equalTo(data.key).on('value',function(noti){
+                            if(noti.numChildren()>0 && Object.values(noti.val())[0].sendTo === currentUserKey){
                                 noti.forEach(function(data){
                                     chkk=data.val()
-                                    if(noti.numChildren()>0 && Object.values(noti.val())[0].sendTo === currentUserKey){
-                                        if(chkk.status === 'Accept'){
-                                            lst += `<li class="list-group-item list-group-item-action">
-                                                <div class="row">
-                                                    <div class="col-md-2">
-                                                        <img src="${user.photoURL}" class="rounded-circle friend-pic" />
-                                                    </div>
-                                                    <div class="col-md-10" style="cursor:pointer;">
-                                                        <div class="name">${user.name}
-                                                            <button class="btn btn-sm btn-default"   style="float:right" ><i class="fas fa-user-plus"></i> Friend</button>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </li>`;
-                                            document.getElementById('lstUsers').innerHTML = lst; 
-                                        }
-                                        else{
-                                        
-                                            lst += `<li class="list-group-item list-group-item-action">
-                                                <div class="row">
-                                                    <div class="col-md-2">
-                                                        <img src="${user.photoURL}" class="rounded-circle friend-pic" />
-                                                    </div>
-                                                    <div class="col-md-10" style="cursor:pointer;">
-                                                        <div class="name">${user.name}
-                                                            <button class="btn btn-sm btn-default"   style="float:right" ><i class="fas fa-user-plus"></i> Pending</button>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </li>`;
-                                            document.getElementById('lstUsers').innerHTML = lst; 
-                                        }
-                                    }
-                                    else{
-                                        lst += `<li class="list-group-item list-group-item-action" data-dismiss="modal" >
+                                    if(chkk.status === 'Accept'){
+                                        lst += `<li class="list-group-item list-group-item-action">
                                             <div class="row">
                                                 <div class="col-md-2">
                                                     <img src="${user.photoURL}" class="rounded-circle friend-pic" />
                                                 </div>
                                                 <div class="col-md-10" style="cursor:pointer;">
                                                     <div class="name">${user.name}
-                                                        <button onclick="sendRequest('${data.key}')" class="btn btn-sm btn-primary" id="sta" style="float:right;"><i class="fas fa-user-plus"></i> Send Request</button>
+                                                        <button class="btn btn-sm btn-default"   style="float:right" ><i class="fas fa-user-plus"></i> Friend</button>
                                                     </div>
                                                 </div>
                                             </div>
                                         </li>`;
-                                        document.getElementById('lstUsers').innerHTML = lst;
+                                        document.getElementById('lstUsers').innerHTML = lst; 
+                                    }
+                                    else{
+                                    
+                                        lst += `<li class="list-group-item list-group-item-action">
+                                            <div class="row">
+                                                <div class="col-md-2">
+                                                    <img src="${user.photoURL}" class="rounded-circle friend-pic" />
+                                                </div>
+                                                <div class="col-md-10" style="cursor:pointer;">
+                                                    <div class="name">${user.name}
+                                                        <button class="btn btn-sm btn-default"   style="float:right" ><i class="fas fa-user-plus"></i> Pending</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>`;
+                                        document.getElementById('lstUsers').innerHTML = lst; 
                                     }
                                 })
-                            })
-                        }
-                    })
+                            }
+                            else{
+                                lst += `<li class="list-group-item list-group-item-action" data-dismiss="modal" >
+                                    <div class="row">
+                                        <div class="col-md-2">
+                                            <img src="${user.photoURL}" class="rounded-circle friend-pic" />
+                                        </div>
+                                        <div class="col-md-10" style="cursor:pointer;">
+                                            <div class="name">${user.name}
+                                                <button onclick="sendRequest('${data.key}')" class="btn btn-sm btn-primary" id="sta" style="float:right;"><i class="fas fa-user-plus"></i> Send Request</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>`;
+                                document.getElementById('lstUsers').innerHTML = lst;
+                            }
+                        })
+                    }
                 })
             }
         });
